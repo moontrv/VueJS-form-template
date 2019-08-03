55f9904cca1f1d19ca62b4c8f1cbb1c46388fad1
Minh N <pesrobot@gmail.com>
2019-08-03 11:50:10 +0300

    1st commit

---
+++ src/SwitchBtn.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div id="on" @click="switched(true)" :class="{active: value}" class="m-mr10">On</div>
+    <div id="off" @click="switched(false)" :class="{active: !value}">Off</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    switched(isOn) {
+      this.$emit("input", isOn);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#on,
+#off {
+  width: 40px;
+  height: 20px;
+  background-color: lightgray;
+  padding: 2px;
+  display: inline-block;
+  margin: 10px -2px;
+  box-sizing: content-box;
+  cursor: pointer;
+  text-align: center;
+}
+
+#on:hover,
+#on.active {
+  background-color: green;
+}
+
+#off:hover,
+#off.active {
+  background-color: red;
+}
+#on {
+  margin-right: 10px;
+}
+</style>
